a7cd47cc01c8c29dd54bd1445eae0ea677ca22c7
Dmitry_Slepchenkov <Dmitry_Slepchenkov@epam.com>
2017-01-23 13:55:19 +0300

    corect commit

--- src/main/java/by/nbrb/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.nbrb.by/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package by.nbrb;
--- src/main/java/org/moneyproj/logic/Action.java
+++
@@ -1,9 +0,0 @@
-package org.moneyproj.logic;
-
-import org.moneyproj.entities.Coin;
-
-public interface Action {
-    Coin performAction(Coin c1, Coin c2, Coin c3);
-
-    String getName();
-}
